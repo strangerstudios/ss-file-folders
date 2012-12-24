4e86ec1c8c14bf3b6827d6c19e8a7d3ef50f8c1b
Jason Coleman <jason@strangerstudios.com>
2012-12-24 14:41:15 -0500

    Added parent and file links.

--- ss-file-folders.php
+++ ss-file-folders.php
@@ -26,38 +26,44 @@
 	$exclude = explode(",", $exclude);
 		
 	// our return string
-	$r = "";
+	$r = "<ul class='ss-file-folders'>";
 	
 	/*
 		Link to Parent
-	*/
+	*/	
+	if(!empty($post->post_parent) && $post->post_parent != $post->ID)
+		$r .= "<li><strong><a href='" . get_permalink($post->post_parent) . "'>..</a></strong></li>";
 	
 	/*
 		Link to sub folders.
 	*/
 		
-	// get posts
-	query_posts(array("post_type"=>"page", "showposts"=>-1, "orderby"=>"menu_order", "post_parent"=>$post->ID, "order"=>"ASC", "post__not_in"=>$exclude));
+	//get posts
+	query_posts(array("post_type"=>$post->post_type, "showposts"=>-1, "orderby"=>"post_title", "post_parent"=>$post->ID, "order"=>"ASC", "post__not_in"=>$exclude));
   
-  	//to show excerpts. save the old value to revert
-	global $more;
-	$oldmore = $more;
-	$more = 0;
-  
-	// the Loop					
+	//the Loop					
 	if ( have_posts() ) : while ( have_posts() ) : the_post();	
-		$r .= '<li><a href="' . get_permalink() . '">' . the_title('','',false) . '</a></li>';            
+		$r .= '<li><strong><a href="' . get_permalink() . '">' . the_title('','',false) . '</a></strong></li>';            
 	endwhile; endif;	
 	
 	//Reset Query
-	wp_reset_query();
-
-	//revert
-	$more = $oldmore;
+	wp_reset_query();	
 	
 	/*
 		Link to files.
 	*/
+	//get posts
+	query_posts(array("post_type"=>"attachment", "post_status" => "inherit", "showposts"=>-1, "orderby"=>"post_title", "post_parent"=>$post->ID, "order"=>"ASC", "post__not_in"=>$exclude));
+  
+	//the Loop					
+	if ( have_posts() ) : while ( have_posts() ) : the_post();	
+		$r .= '<li><a href="' . get_the_guid() . '">' . the_title('','',false) . '</a></li>';            
+	endwhile; endif;	
+	
+	//Reset Query
+	wp_reset_query();
+	
+	$r .= "</ul>";
 	
 	return $r;
 }
