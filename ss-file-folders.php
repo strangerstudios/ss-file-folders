834a82eec3268407e4657979a5baf644027ae8e8
Jason Coleman <jason@strangerstudios.com>
2013-03-05 11:51:45 -0500

    ...

--- ss-file-folders.php
+++ ss-file-folders.php
@@ -8,32 +8,67 @@
 	Author URI: http://www.strangerstudios.com
 	*/
 	
+	global $ssff_options;
+	$ssff_options['display'] = "table";
+	
 	//shortcode function
 	function ssff_shortcode_handler($atts, $content=null, $code="") {
-	global $post;
-	
+	global $post, $ssff_options;
+			
 	// $atts    ::= array of attributes
 	// $content ::= text within enclosing form of shortcode element
 	// $code    ::= the shortcode found, when == callback name
-	// examples: [subpagelist exclude="1,2,3"]
+	// examples: [file-folders]
 	
+	//no params yet, but keeping this here for reference in case we do
 	extract(shortcode_atts(array(
 		'exclude' => NULL
 	), $atts));
 		
-	// prep exclude array
-	$exclude = str_replace(" ", "", $exclude);
-	$exclude = explode(",", $exclude);
-		
+	//make sure we have our	css loaded
+	wp_enqueue_style("ss-file-folders", plugins_url("css/ss-file-folders.css", __FILE__), NULL, ".1");
+	
+	
 	// our return string
-	$r = "<ul class='ss-file-folders'>";
+	
+	/*
+		Breadcrumbs
+	*/	
+	$r = "<p class='breadcrumbs'>";
+	
+	$breadcrumbs = get_post_ancestors($post->ID);				
+	if($breadcrumbs)
+	{
+		$breadcrumbs = array_reverse($breadcrumbs);
+		foreach ($breadcrumbs as $crumb)
+		{
+			$r .= '<a href="' . get_permalink($crumb) . '">' . get_the_title($crumb) . '</a>&nbsp;&nbsp;&raquo;&nbsp;&nbsp;';
+		}
+		$r .=  $post->post_title;
+	}	
+	
+	$r .= '</p>';
+		
+	if($ssff_options['display'] == "table")
+		$r .= "<table class='ss-file-folders'><thead><tr><th width='50'><!--thumb--></th><th>Name</th><th>File</th><th>Date Modified</th></tr></thead><tbody>";
+	else
+		$r .= "<ul class='ss-file-folders'>";
 	
 	/*
 		Link to Parent
-	*/	
+	*/
 	if(!empty($post->post_parent) && $post->post_parent != $post->ID)
-		$r .= "<li><strong><a href='" . get_permalink($post->post_parent) . "'>..</a></strong></li>";
-	
+	{
+		if($ssff_options['display'] == "table")
+		{			
+			$r .= "<tr><td class='ss-file-folders_thumb'><a href='" . get_permalink($post->post_parent) . "'><img src='" . plugins_url("images/icon_folder-prev.png", __FILE__) . "' width='32' /></a></td><td class='ss-file-folders_prev' colspan='3'><a href='" . get_permalink($post->post_parent) . "'>..</a></td></tr>";
+		}
+		else
+		{
+			$r .= "<li class='ss-file-folders_prev'><strong><a href='" . get_permalink($post->post_parent) . "'>..</a></strong></li>";
+		}
+	}
+
 	/*
 		Link to sub folders.
 	*/
@@ -43,7 +78,26 @@
   
 	//the Loop					
 	if ( have_posts() ) : while ( have_posts() ) : the_post();	
-		$r .= '<li><strong><a href="' . get_permalink() . '">' . the_title('','',false) . '</a></strong></li>';            
+		if($ssff_options['display'] == "table")
+		{
+			$r .= "<tr>";
+			
+			$r .= "<td class='ss-file-folders_thumb'>";
+			$r .= '<a href="' . get_permalink() . '"><img src="' . plugins_url("images/icon_folder-explore.png", __FILE__) . '" width="32" /></a>';			
+			$r .= "</td>";
+			
+			$r .= '<td colspan="2">
+				<strong><a href="' . get_permalink() . '">' . the_title('','',false) . '</a></strong>					
+			</td>';
+									
+			$r .= '<td>' . get_the_modified_date("m/d/Y");
+					
+			$r .= "</td>";
+
+			$r .= '</tr>';
+		}
+		else
+			$r .= '<li><strong><a href="' . get_permalink() . '">' . the_title('','',false) . '</a></strong></li>';            
 	endwhile; endif;	
 	
 	//Reset Query
@@ -57,16 +111,100 @@
   
 	//the Loop					
 	if ( have_posts() ) : while ( have_posts() ) : the_post();	
-		$r .= '<li><a href="' . get_the_guid() . '">' . the_title('','',false) . '</a></li>';            
+		if($ssff_options['display'] == "table")
+		{
+			$r .= "<tr>";
+			
+			$r .= "<td class='ss-file-folders_thumb'>";
+			if($thumb = wp_get_attachment_image($post->ID, array( 32, 32 ), true))
+				$r .= '<a href="' . wp_get_attachment_url($post->ID) . '">' . $thumb . '</a>';			
+			
+			$r .= "</td>";
+			
+			$r .= '<td>					
+				<a href="' . wp_get_attachment_url($post->ID) . '">' . the_title('','',false) . '</a>';
+			
+			$r .= "</td>";
+			
+			$r .= '<td>		
+				<a href="' . wp_get_attachment_url($post->ID) . '" class="ssff-file-link">' . ssff_getFilenameFromGuid($post->guid) . '</a>';
+			
+			$r .= "</td>";
+			
+			$r .= '<td>' . date("m/d/Y", strtotime($post->post_date));
+					
+			$r .= "</td>";
+			
+			$r .= '</tr>';						
+		}
+		else
+			$r .= '<li><a href="' . get_the_guid() . '">' . the_title('','',false) . '</a></li>';            
 	endwhile; endif;	
 	
 	//Reset Query
 	wp_reset_query();
 	
-	$r .= "</ul>";
+	if($ssff_options['display'] == "table")
+		$r .= "</tbody></table>";
+	else
+		$r .= "</ul>";
 	
 	return $r;
 }
-
 add_shortcode('file-folders', 'ssff_shortcode_handler');
-?>
+
+/*
+	Sets the "main_post_id" so we know which page we are looking at.
+*/
+function ssff_wp()
+{
+	global $post, $ssff_options;	
+	if(!empty($post->ID))
+		$ssff_options['main_post_id'] = $post->ID;
+}
+add_action("wp", "ssff_wp");
+
+/*
+	So we know if we are outputting the body yet or not
+*/
+function ssff_body_class($class)
+{
+	global $post, $ssff_options;
+	
+	if(strpos($post->post_content, "[file-folders]") !== false)
+	{
+		$ssff_options['in_body'] = true;
+		$class[] = "page-ss-file-folders";
+	}
+		
+	return $class;
+}
+add_filter("body_class", "ssff_body_class");
+
+/*
+	Function to change title of page to Files
+*/
+function ssff_the_title($title, $post_id)
+{
+	global $ssff_options;	
+	if(!empty($ssff_options['in_body']) && $post_id == $ssff_options['main_post_id'])
+	{
+		$title = "Files";
+	}
+	
+	return $title;
+}
+//add_action("the_title", "ssff_the_title", 10, 2);
+	
+/*
+	Gets a filename from a guid
+*/
+function ssff_getFilenameFromGuid($guid)
+{
+	//generally we want everything after the last slash
+	$parts = explode("/", $guid);
+	if($parts)
+		return $parts[count($parts) - 1];
+	else
+		return $guid;
+}		
